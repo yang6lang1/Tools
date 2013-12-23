9ebe0da85829c08b24bd28c2573d170f656b8946
alex.yang <alex.yang@WRK-IT-07.zepower.com>
2013-12-23 15:11:34 -0800

    Text reader finished

--- Tools/src/input/TXTReader.java
+++ Tools/src/input/TXTReader.java
@@ -1,20 +1,24 @@
 package input;
 
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-/**Read the input from txt
+/**
+ * Text reader: save to be used for project deployment.
  * */
 public class TXTReader {
-
+  
   private String filename;
   private BufferedReader reader;
   
   public TXTReader(String filename) throws FileNotFoundException{
 	this.filename = filename;
-	reader = new BufferedReader(new FileReader(filename));
+	InputStream in = getClass().getResourceAsStream(filename);
+	reader = new BufferedReader(new InputStreamReader(in));
   }
   
   public String getInputAsOneString() throws IOException{
@@ -26,6 +30,15 @@ public class TXTReader {
 	return output;
   }
 
+  public String getInputAsOneStringRetainLineBreaker() throws IOException{
+	String output = "";
+	String textline = "";
+	while((textline = reader.readLine()) != null){
+	  output += textline+"\n";
+	}
+	return output;
+  }
+  
   public String getFilename() {
 	return filename;
   }
