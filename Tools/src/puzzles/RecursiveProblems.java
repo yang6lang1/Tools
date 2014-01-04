d9c112110e9c90c17509bf37475c5c424c5a6e8b
alex.yang <alex.yang@WRK-IT-07.zepower.com>
2014-01-03 16:49:20 -0800

    Yayyyy! Last day at work!

--- Tools/src/puzzles/RecursiveProblems.java
+++ Tools/src/puzzles/RecursiveProblems.java
@@ -22,6 +22,95 @@ public class RecursiveProblems {
 	  return mid;
 	}
   }
+  
+  public final static int East = 1;
+  public final static int South = 2;
+  public final static int West = 3;
+  public final static int North = 4;
+  
+  private int[][] mat;
+  public String printMat(){
+	if(mat == null) return "Empty matrix";
+	
+	String out = "";
+	for(int n = 0; n < mat.length; n++){
+	  for(int m = 0; m < mat[n].length; m++){
+		out += "["+(n+1)+","+(m+1)+"]:" + mat[n][m]+"\t\t ";
+	  }
+	  out += "\n";
+	}
+	
+	return out;
+  }
+  
+  public void buildSpiralMatrix(int m, int n){
+	//TODO: special case
+	int[][] matrix = new int[n][m];
+	this.fillMatrixClockwisely(matrix, -1, 0, 0, East, m);
+	mat = matrix;
+  }
+  
+  private void fillMatrixClockwisely(int[][] matrix, int startM, int startN, int startCounter, int direction, int mLength){
+	//TODO: special case: startM / startN = 0?
+	if(mLength == 0) return;
+	if(direction > North) return;
+	
+	int counter = startCounter;
+	int n = startN, m = startM;
+	int loopCounter = matrix[0].length - mLength;
+	for(int i = 0; i < mLength; i++){
+	  switch(direction){
+	  case East:
+		m ++;
+		break;
+	  case South:
+		n ++;
+		break;
+	  case West:
+		m --;
+		break;
+	  case North:
+		n --;
+		break;
+	  default:
+		break;
+	  }
+	  
+	  matrix[n][m] = counter;
+	  counter++;
+	}	
+	
+	direction++;
+	if(direction > North) direction = East;
+	int nLength = matrix.length - loopCounter - 1;
+	if (nLength == 0) return;
+	for(int i = 0; i < nLength; i++){
+	  switch(direction){
+	  case East:
+		m ++;
+		break;
+	  case South:
+		n ++;
+		break;
+	  case West:
+		m --;
+		break;
+	  case North:
+		n --;
+		break;
+	  default:
+		break;
+	  }
+	  
+	  matrix[n][m] = counter;
+	  counter++;
+	}
+	
+	//next iteration:
+	direction++;
+	if(direction > North) direction = East;
+	this.fillMatrixClockwisely(matrix, m, n, counter, direction, mLength - 1);
+  }
 
   public static void main(String[] args) {
 	int[] sortedArray = new int[20];
@@ -38,5 +127,10 @@ public class RecursiveProblems {
 	else{
 	  System.out.println("index: "+ index);
 	}
+	
+	System.out.println();
+	System.out.println("fill matrix: ");
+	test.buildSpiralMatrix(5, 5);
+	System.out.println(test.printMat());
   }
 }
